8c7a6362379eb1bac871307a550c7f46de580eec
MD. ASAD-AL-ADIL <adilasadals@gmail.com>
2026-01-18 04:13:57 +0600

    updated

---
+++ Exams/6. AL-ML/2. Machine Learning Course/4. Final Exam/app.py
@@ -0,0 +1,55 @@
+import gradio as gr
+import pandas as pd
+import pickle
+import numpy as np
+
+# 1. Load the Model
+with open("Medical_Charges_model.pkl", "rb") as f:
+    model=pickle.load(f)
+
+# 2. logic interface
+def predict_charges(age,gender,weight,feet,inch,children,smoker,region,income):
+    h=(feet*12+inch)*(2.54/100) # inch to meter conversion
+    bmi=weight/(h**2)
+    
+    if income<0:
+        return "Income Must be positive"
+    
+    if age<18:
+        age_group='Child'
+    elif age>=18 and age<50:
+        age_group='Adult'
+    else:
+        age_group='Senior'
+    
+    input_df=pd.DataFrame(
+        [[age,gender,bmi,children,smoker,region,age_group,income]],
+      columns=[
+        'age','sex','bmi','children','smoker','region','age_group','income'
+    ]
+      )
+    
+    prediction=np.exp(model.predict(input_df)[0])
+    
+    return f"Predicted Medical Charges: {np.round(prediction)}"
+
+# 3. The App Interface
+inputs=[
+    gr.Number(label="Age",value=18),
+    gr.Radio(["male","female"], label="Gender"),
+    gr.Number(label="Weight(kg)"),
+    gr.Number(label="Height(Feet)"),
+    gr.Number(label="Height(Inch)"),
+    gr.Dropdown([i for i in range(0,16)], label="Number of Childs"),
+    gr.Dropdown(['southwest','southeast','northwest','northeast'], label="Region"),
+    gr.Radio(["Yes", "No"], label="Smoker"),
+    gr.Number(label="Income"),
+]
+
+app=gr.Interface(
+    fn=predict_charges,
+      inputs=inputs,
+        outputs="text", 
+        title="Medical Charges Predictor")
+
+app.launch()
